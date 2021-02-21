1cc5c32a62396a1c582864f5deea04cda3c140fe
Johannes Ferreira <johannesssf@gmail.com>
2021-02-21 14:41:29 -0300

    Create api project

---
+++ physical_legal_goods/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for physical_legal_goods project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'physical_legal_goods.settings')
+
+application = get_asgi_application()
