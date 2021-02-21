a2716737981d814a7c37f60e43667f96c066ad44
Johannes Ferreira <johannesssf@gmail.com>
2021-02-21 15:12:16 -0300

    Create physical person model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class PhysicalPerson(models.Model):
+    cpf = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=8)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=12)
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import PhysicalPerson
+
+
+class PhysicalPersonModel(TestCase):
+    def test_model_create(self):
+        physical_person = PhysicalPerson.objects.create(
+            cpf="25845675391",
+            name="Fulando Mais Sobrenome",
+            zipcode="12345678",
+            email="fulano@email.com",
+            phone_number="11234567890"
+        )
+        self.assertIsNotNone(physical_person)
